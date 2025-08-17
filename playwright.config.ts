bf570b481b958f9c8e13d126120f21d597006ec8
Porostik <porostik2@yandex.ru>
2025-08-18 01:41:36 +0600

    Add test retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: 'e2e',
-  retries: 0,
+  retries: 2,
   use: {
     baseURL: 'http://localhost:5173/starwars',
     headless: true,
